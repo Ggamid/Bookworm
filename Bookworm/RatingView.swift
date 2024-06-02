c3e87859943e9571b76becac7aa3b80b547ca697
Гамид Халидов <khalidov-gamid@mail.ru>
2024-06-02 23:18:48 +0300

    BookList, RatingView, EmojiRatingView, New genres

---
+++ Bookworm/RatingView.swift
@@ -0,0 +1,18 @@
+//
+//  RatingView.swift
+//  Bookworm
+//
+//  Created by Gamıd Khalıdov on 01.06.2024.
+//
+
+import SwiftUI
+
+struct RatingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RatingView()
+}
